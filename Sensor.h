7d0db3f9223a004004ca1dbf4f57861cd19caa12
rafaelaBH <rafaela.b@campus.technion.ac.il>
2026-03-21 22:09:17 +0200

    initial commit - Sensor classes + Kalman filter

---
+++ Sensor.h
@@ -0,0 +1,46 @@
+
+#ifndef SENSOR_H
+#define SENSOR_H
+
+
+
+class Sensor {
+
+  private:
+    double value;
+    double error;
+    double estimatedValue;
+    double var;
+
+    double addNoise();
+    double kalmanFilter();
+
+  public:
+    explicit Sensor(double val = 0, double err = 1, double est = 0, double v = 0.7) :
+    value(val), error(err), estimatedValue(est), var(v) {}
+    Sensor(Sensor& s) = delete;
+    virtual ~Sensor() = default;
+    virtual void readValue();
+
+  protected:
+    double getValue() const { return value;}
+    void setValue(const double newVal) { value = newVal;}
+};
+
+class TemperatureSensor : public Sensor {
+
+  public:
+    TemperatureSensor() : Sensor(0, 0.8, 25.8, 0.9) {}
+    ~TemperatureSensor() override = default;
+};
+
+class HumiditySensor : public Sensor {
+
+  public:
+    HumiditySensor() : Sensor(0, 0.9, 50.3, 0.5) {}
+    ~HumiditySensor() override = default;
+};
+
+
+
+#endif //SENSOR_H
