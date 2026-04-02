3d833191779f9f2e71192e05be2caa6e10c53fa0
rafaelaBH <rafaela.b@campus.technion.ac.il>
2026-04-03 01:32:52 +0300

    Fix main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 int main() {
-    // Seed for the random flaws in your Sensor class
+    // Seed for the random flaws in Sensor class
     srand(time(0));
 
     ControllerTest::RunAllTests();
